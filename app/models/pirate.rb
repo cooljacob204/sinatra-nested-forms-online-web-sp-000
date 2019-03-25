fb2c16cbf06f87334936bdfae9612908efb70714
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-25 20:14:05 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,3 +1,13 @@
 class Pirate
   attr_accessor :name, :weight, :height
+  @@all = []
+  
+  def initialize(name='', weight=0, height=0)
+    @name = name
+    @weight = weight
+    @height = height
+    
+    @@all << self
+  end
+  
 end
