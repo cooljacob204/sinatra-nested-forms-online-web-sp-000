7949adb92484eb7442bbf4273f25b02a486952e7
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-25 20:41:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,11 +14,11 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
       
-      params[:pirate][:ships].each do |ship|
-        Ship.new(ship)
-      end
+      # params[:pirate][:ships].each do |ship|
+      #   Ship.new(ship)
+      # end
       
-      @ships = Ship.all
+      # @ships = Ship.all
       
       erb :'pirates/show'
     end
