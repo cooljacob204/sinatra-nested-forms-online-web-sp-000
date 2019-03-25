d96d53e9a3a444651f4b506c9bcbe87c2a36af59
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-25 20:15:59 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,15 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  
+  class << self 
+    def all
+      @@all
+    end
+    
+    def clear
+      @@all = []
+    end
+  end
   
 end
